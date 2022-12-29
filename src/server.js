4260e672bdd6981d059967cca4b541901111b6fb
suhye0n <contact.suhyeon@gmail.com>
2022-12-29 18:09:00 +0900

    221229 added WebWocket Events

--- src/server.js
+++ src/server.js
@@ -7,12 +7,18 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
