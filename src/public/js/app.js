c1ba5c076d7977e9023042e61d4dcffd5b81345b
suhye0n <contact.suhyeon@gmail.com>
2023-01-01 01:14:21 +0900

    2023년 첫 커밋: SocketIO 사용 :sparkles:

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,15 @@
 const socket = io();
 
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-// const messageList = document.querySelector("ul");
-// const nickForm = document.querySelector("#nick");
-// const messageForm = document.querySelector("#message");
-// const socket = new WebSocket(`ws://${window.location.host}`);
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
+  input.value = "";
+}
 
-// function makeMessage(type, payload) {
-//     const msg = {type, payload};
-//     return JSON.stringify(msg);
-// }
-
-// function handleOpen() {
-//     console.log("Connected to Server");
-// }
-
-// socket.addEventListener("open", handleOpen);
-
-// socket.addEventListener("message", (message) => {
-//     const li = document.createElement("li");
-//     li.innerText = message.data;
-//     messageList.append(li);
-// });
-
-// socket.addEventListener("close", () => {
-//     console.log("DisConnected from Server");
-// });
-
-// function handleSubmit(event) {
-//     event.preventDefault();
-//     const input = messageForm.querySelector("input");
-//     socket.send(makeMessage("new_message", input.value));
-//     // const li = document.createElement("li");
-//     // li.innerText = `나: ${input.value}`;
-//     input.value = "";
-// }
-
-// function handleNickSubmit(event) {
-//     event.preventDefault();
-//     const input = nickForm.querySelector("input");
-//     socket.send(makeMessage("nickname", input.value));
-// }
-
-// messageForm.addEventListener("submit", handleSubmit);
-// nickForm.addEventListener("submit", handleNickSubmit)
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,50 +1,24 @@
 import http from "http";
 import SocketIO from "socket.io";
-// import WebSocket from "ws";
 import express from "express";
 
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const httpserver = http.createServer(app);
-const wsServer = SocketIO(httpserver);
-
-wsServer.on("connection", socket => {
-    console.timeLog(socket);
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+          done();
+        }, 10000);
+    });
 });
 
-// const wss = new WebSocket.Server({server});
-
-// function onSocketClose() {
-//     console.log("Disconnected from the Browser")
-// }
-
-// const sockets= [];
-
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "익명";
-//     console.log("Connected to Browser");
-//     socket.on("close", onSocketClose)
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch(message.type) {
-//             case "new_message":
-//                 sockets.forEach((aSocket) =>
-//                     aSocket.send(`${socket.nickname}: ${message.payload}`)
-//                 );
-//                 break;
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//                 break;
-//         }
-//     });
-// });
-
-const handleListen = () => console.log(`Listening on http://localhost:3000`)
-httpserver.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
