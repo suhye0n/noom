df9cf877b3ef9d8e325480da9f64dbffedddc3ca
suhye0n <contact.suhyeon@gmail.com>
2023-01-01 01:42:30 +0900

    230101 방 참가 기능 생성

--- src/server.js
+++ src/server.js
@@ -3,20 +3,23 @@ import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
-        setTimeout(() => {
-          done();
-        }, 10000);
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
     });
 });
 
