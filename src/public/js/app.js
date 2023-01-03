663348670013b59efc660d8bcea39629bd21e467
suhye0n <contact.suhyeon@gmail.com>
2023-01-04 00:32:35 +0900

    230104 Camera Switch & Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,40 +4,55 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 async function getCameras() {
     try {
       const devices = await navigator.mediaDevices.enumerateDevices();
       const cameras = devices.filter((device) => device.kind === "videoinput");
+      const currentCamera = myStream.getVideoTracks()[0];
       cameras.forEach((camera) => {
         const option = document.createElement("option");
         option.value = camera.deviceId;
         option.innerText = camera.label;
+        if (currentCamera.label === camera.label) {
+            option.selected = true;
+        }
         camerasSelect.appendChild(option);
       });
     } catch (e) {
       console.log(e);
     }
-  }
-
-async function getMedia() {
-  try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
-    myFace.srcObject = myStream;
-    await getCameras();
-  } catch (e) {
-    console.log(e);
-  }
 }
-  
-getMedia();
+
+async function getMedia(deviceId) {
+    const initialConstrains = {
+      audio: true,
+      video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+      audio: true,
+      video: { deviceId: { exact: deviceId } },
+    };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstrains
+        );
+        myFace.srcObject = myStream;
+        if (!deviceId) {
+            await getCameras();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 function handleMuteClick() {
     myStream
@@ -51,7 +66,6 @@ function handleMuteClick() {
       muted = false;
     }
 }
-
 function handleCameraClick() {
     myStream
     .getVideoTracks()
@@ -65,5 +79,33 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+socket.on("welcome", () => {
+  console.log("someone joined");
+});
